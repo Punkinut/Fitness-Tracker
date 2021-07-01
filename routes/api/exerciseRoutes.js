9e77087320c8f88c71f761538d93ec21a83cb215
Punkinut <heypunkinut@gmail.com>
2021-07-01 14:08:03 +1000

    Added basic get request to get workout data from the database

--- routes/api/exerciseRoutes.js
+++ routes/api/exerciseRoutes.js
@@ -1,3 +1,15 @@
-const router = require('express').Router();
 // The front-end api file will give you a large hint to what back-end request you will have to do here
+const router = require('express').Router();
+const db = require('../../models');
+
+router.get('/', (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+})
+
 module.exports = router;
