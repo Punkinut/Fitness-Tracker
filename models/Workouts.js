02a7d893b3727691cbd7cb2f9bb763eb4cfc03d1
Punkinut <heypunkinut@gmail.com>
2021-07-01 13:10:08 +1000

    Setup basic database, model and seeding now works

--- models/Workouts.js
+++ models/Workouts.js
@@ -4,9 +4,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number,
+        },
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
+    }
 })
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workouts", WorkoutSchema);
 
 module.exports = Workout;
