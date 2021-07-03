5449d4cb9ac66258fabf0b60f43f877b14f75638
Punkinut <heypunkinut@gmail.com>
2021-07-03 15:12:02 +1000

    Changed colour of graphs

--- public/js/stats.js
+++ public/js/stats.js
@@ -40,8 +40,8 @@ function populateChart(data) {
       datasets: [
         {
           label: 'Workout Duration In Minutes',
-          backgroundColor: 'red',
-          borderColor: 'red',
+          backgroundColor: '#448152',
+          borderColor: '#448152',
           data: durations,
           fill: false,
         },
@@ -69,22 +69,8 @@ function populateChart(data) {
         {
           label: 'Pounds',
           data: pounds,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
+          backgroundColor: '#448152',
+          borderColor: '#448152',
           borderWidth: 1,
         },
       ],
